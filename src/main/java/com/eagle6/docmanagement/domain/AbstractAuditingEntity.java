f83296c627f17b0402da2a3beaffc8793b0ea268
anklavez@gmail.com <1qa2wsQAZwsx>
2018-07-26 14:35:09 +0400

    Remove mongodb

--- src/main/java/com/eagle6/docmanagement/domain/AbstractAuditingEntity.java
+++ src/main/java/com/eagle6/docmanagement/domain/AbstractAuditingEntity.java
@@ -4,10 +4,8 @@ import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.mapping.Field;
 import java.time.Instant;
 
 
@@ -20,22 +18,17 @@ public abstract class AbstractAuditingEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @CreatedBy
-    @Field("created_by")
     @JsonIgnore
     private String createdBy;
 
-    @CreatedDate
-    @Field("created_date")
     @JsonIgnore
     private Instant createdDate = Instant.now();
 
     @LastModifiedBy
-    @Field("last_modified_by")
     @JsonIgnore
     private String lastModifiedBy;
 
     @LastModifiedDate
-    @Field("last_modified_date")
     @JsonIgnore
     private Instant lastModifiedDate = Instant.now();
 
